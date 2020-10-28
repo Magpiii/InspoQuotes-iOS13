fc127d19f1535b5e41bc7bcfd973f023c1727a5f
Hunter Hudson <hunterhudson3@gmail.com>
2020-10-28 13:17:24 -0600

    Add Constants file, configure tableView, add premium quotes purchase functionality.

---
+++ InspoQuotes/Controllers/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  InspoQuotes
+//
+//  Created by Hunter Hudson on 10/28/20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K{
+    static let cellIdentifier = "QuoteCell"
+}
--- InspoQuotes/Controllers/QuoteTableViewController.swift
+++ InspoQuotes/Controllers/QuoteTableViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
+//Must import StoreKit for in-app purchases:
+import StoreKit
+
 class QuoteTableViewController: UITableViewController {
+    //ProductID from Apple:
+    
+    let productID = "[Product ID from Apple]"
     
     var quotesToShow = [
         "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
@@ -30,7 +36,13 @@ class QuoteTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //Sets the tableView's delegate as self:
+        tableView.delegate = self
+        
+        //Sets the PaymentQueue delegate method to self (NOTE: this is required):
+        SKPaymentQueue.default().add(self)
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -47,15 +59,36 @@ class QuoteTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        /*Returns the amount of rows equal to the amount of quotes in the quotesToShow array (+1 for the prompt to do in-app purchase):
+        */
+        return quotesToShow.count + 1
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        
+        //If the current indexPath is less than the amount of quotes...
+        if (indexPath.row < quotesToShow.count){
+            //...allows the textLabel to use as many lines as it needs to show the quote:
+            cell.textLabel?.numberOfLines = 0
+            
+            //Sets the cell's textLabel to the appropriate quote for the index of the array:
+            cell.textLabel?.text = quotesToShow[indexPath.row]
+        //Otherwise...
+        } else {
+            //Sets the text label of the new cell to the following string:
+            cell.textLabel?.text = "Tap here to get more quotes!"
+            
+            cell.textLabel?.numberOfLines = 0
+            cell.textLabel?.textColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+            
+            /*Puts a chevron at the right side of the cell which tells the user that the cell will take them somewhere if they tap on it:
+            */
+            cell.accessoryType = .disclosureIndicator
+        }
+        
         return cell
     }
     
@@ -105,13 +138,69 @@ class QuoteTableViewController: UITableViewController {
     }
     */
     
+    //MARK: - TableView Delegate Methods:
+    //Method for when cell is selected:
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //If the current index of the array equals the end of the quotesToShow array...
+        if (indexPath.row == quotesToShow.count){
+            //Checks to make sure the cell I'm working with is actually the right cell:
+            print("Buy quotes tapped.")
+            
+            buyPremiumQuotes()
+        }
+        
+        /*Deselects the row after tapping so that it doesn't stay "tapped" (UI looks better that way):
+        */
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
-    
-    
+    //MARK: - In-app Purchase Methods:
+    func buyPremiumQuotes() {
+        /*Condition check to make sure the user has the permissions to actually make purchases (i.e. doesn't have parental controls):
+        */
+        if SKPaymentQueue.canMakePayments() == true {
+            //Initializes a new payment request:
+            let paymentRequest = SKMutablePayment()
+            
+            //Specifies the product identifier for the purchase as the productID from Apple:
+            paymentRequest.productIdentifier = productID
+            
+            /*Adds the above payment request to the PaymentQueue (works kind of like DispatchQueue):
+            */
+            SKPaymentQueue.default().add(paymentRequest)
+            
+        } else {
+            print("Error code 1: user is unable to make payments.")
+        }
+    }
     
     @IBAction func restorePressed(_ sender: UIBarButtonItem) {
         
     }
+}
 
-
+//MARK: - Transaction Observer:
+extension QuoteTableViewController: SKPaymentTransactionObserver{
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        /*Transactions are processed as an array, so one must loop through the transactions to see if the user actually paid:
+        */
+        for transaction in transactions {
+            //If the user paid for it and the transaction went through...
+            if (transaction.transactionState == .purchased){
+                print("Transaction succeeded.")
+                
+                /*Indicates to the PaymentQueue that the transaction is complete for the current transaction:
+                */
+                SKPaymentQueue.default().finishTransaction(transaction)
+            /*Else if the user's transaction failed (i.e. they hit cancel or the card got declined)...
+            */
+            } else if (transaction.transactionState == .failed){
+                print("Error code 2: transaction failed.")
+                
+                /*Must indicate that transaction is finished in both cases, whether it succeeds or not:
+                */
+                SKPaymentQueue.default().finishTransaction(transaction)
+            }
+        }
+    }
 }
